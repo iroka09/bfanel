872fe93b1a7d03f85d5dab9e35055309fcad478d
Iroka Ntomchukwu Chisom <irokirokatochukwu@gmail>
2026-02-16 18:08:30 +0100

    yes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <>
       <div className="p-5 mb-5">
-        <Image src="/logo_high.png" alt="bfanel logo" height="200" width="300" className="object-contain mb-4" />
+        <Image src="/logo_high.png" alt="bfanel logo" height="200" width="300" className="object-contain mb-4 mx-auto" />
         <AboutUs />
       </div>
       <Footer />
